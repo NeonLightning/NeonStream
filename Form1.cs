8517a0fb3061e5b398cd03356233ed8f121e029f
NeonLightning <neonlightning@gmail.com>
2022-07-14 19:13:04 -0400

    trying to use shojy's wonderful work to figure out how to read memory.

--- Form1.cs
+++ Form1.cs
@@ -3,65 +3,35 @@ using Shojy.FF7.Elena.Converters;
 using Shojy.FF7.Elena;
 using System.Diagnostics;
 using System.Drawing.Imaging;
+using NeonStream.Constants;
 
 namespace NeonStream
 {
     public partial class Form1 : Form
     {
+        private static Process FF7 { get; set; }
+        private static NativeMemoryReader MemoryReader { get; set; }
+        private static FF7SaveMap SaveMap { get; set; }
+        public static FF7BattleMap BattleMap { get; set; }
+
         public Form1()
         {
             InitializeComponent();
             PortableSettingsProvider.ApplyProvider(Properties.Settings.Default);
             Starting.Text = ("starting...");
             Process ff7 = null;
-            do
-            {
-                try
-                {
-                    ff7 ??= Process.GetProcessesByName("ff7_en").FirstOrDefault() ?? Process.GetProcessesByName("ff7").FirstOrDefault();
-                }
-                catch
-                {
-                    // Wait and try again
-                    Thread.Sleep(250);
-                }
+            MemoryReader = new NativeMemoryReader(FF7);
+            var saveMapByteData = MemoryReader.ReadMemory(new IntPtr(Addresses.SaveMapStart), 4342);
+            var isBattle = MemoryReader.ReadMemory(new IntPtr(Addresses.ActiveBattleState), 1).First();
+            var battleMapByteData = MemoryReader.ReadMemory(new IntPtr(Addresses.BattleMapStart), 0x750);
+            var colors = MemoryReader.ReadMemory(new IntPtr(Addresses.WindowColorBlockStart), 16);
+            SaveMap = new FF7SaveMap(saveMapByteData);
+            BattleMap = new FF7BattleMap(battleMapByteData, isBattle);
 
-            } 
-            while (ff7?.MainModule is null) ;
-
-            var ff7Folder = Path.GetDirectoryName(ff7.MainModule.FileName);
-
-            var path = Path.Combine(ff7Folder, "data", "lang-en", "kernel");
-            var minigame = Path.Combine(ff7Folder, "data", "minigame");
-
-            // Read Kernel Data
-            var reader = new KernelReader(Path.Combine(path, "KERNEL.BIN"))
-                .MergeKernel2Data(Path.Combine(path, "kernel2.bin"));
-
-            var weapons = reader.WeaponData.Weapons;
-
-            int temp = 0;
-            foreach (var wpn in weapons)
-            {
-                temp++;
-                //Console.WriteLine($"Found a {wpn.Name}!");
-                if (temp == 0)
-                {
-                    var wep1text = wpn.Name;
-                    wep1label.Text = wep1text;
-                }
-                if (temp == 1)
-                {
-                    wep2label.Text = ("Weapon");
-                }
-                if (temp == 2)
-                {
-                    wep3label.Text = ("Weapon");
-                }
-            }
-            temp = 0;
-            var mem = new MemoryKernelReader(ff7);
-
+            SaveMap.WindowColorTopLeft = $"{colors[0x2]:X2}{colors[0x1]:X2}{colors[0x0]:X2}";
+            SaveMap.WindowColorBottomLeft = $"{colors[0x6]:X2}{colors[0x5]:X2}{colors[0x4]:X2}";
+            SaveMap.WindowColorTopRight = $"{colors[0xA]:X2}{colors[0x9]:X2}{colors[0x8]:X2}";
+            SaveMap.WindowColorBottomRight = $"{colors[0xE]:X2}{colors[0xD]:X2}{colors[0xC]:X2}";
         }
     }
 }
